8d93e67cef5427415aa4123f14753cd6b84043dc
Damian Labudek <damian.labudek@gmail.com>
2025-09-05 09:53:15 +0100

    prank

--- pswu.ps1
+++ pswu.ps1
@@ -5,7 +5,7 @@ Install-WindowsUpdate -MicrosoftUpdate -NotKBArticleID KB5063878 -AcceptAll -Aut
 Write-Output 'Windows Update has finished.'
 Add-Type -AssemblyName System.Speech
 $synthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$text = "Windows update has finished."
+$text = "Windows is ready and waiting Callum"
 $synthesizer.Speak($text)
 Set-ExecutionPolicy -ExecutionPolicy Default
 '@
