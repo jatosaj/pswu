3be14f9046ba3ddbce5c794b73f6bc4182ee8db7
Damian Labudek <damian.labudek@gmail.com>
2026-03-03 13:45:22 +0000

    changed speech to living in a dream

--- pswu.ps1
+++ pswu.ps1
@@ -5,7 +5,7 @@ Install-WindowsUpdate -MicrosoftUpdate -NotKBArticleID KB5063878 -AcceptAll -Aut
 Write-Output 'Windows Update has finished.'
 Add-Type -AssemblyName System.Speech
 $synthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$text = "Happy Friday!"
+$text = "Living in a dream!"
 $synthesizer.Speak($text)
 #Set-ExecutionPolicy -ExecutionPolicy Default
 Start-Sleep -Seconds 10
